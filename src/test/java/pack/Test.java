309dd4af6fb0e9c04cd54cd4091c0ffa8d73ce08
Mosarrof Hossain <phmosharaf@gmail.com>
2021-01-23 10:04:47 -0600

    Few code added in the test class

--- src/test/java/pack/Test.java
+++ src/test/java/pack/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing Git");
+		
+		System.out.println("Testing Git Branch");
+		System.out.println("Testing");
+		System.out.println("Testing git branch in github");
 	}
 
 }
