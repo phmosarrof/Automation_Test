abc3f69f4c3ef818d6f10ccf70c900321077f64a
mdmohsin99 <71600972+mdmohsin99@users.noreply.github.com>
2021-01-30 11:07:32 -0500

    PageScroll

---
+++ src/test/java/pageScroll/ScrollPage.java
@@ -0,0 +1,28 @@
+package pageScroll;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScrollPage {
+
+	public static void main(String[] args) throws Throwable {
+		
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.toolsqa.com/selenium-webdriver/window-handle-in-selenium/");
+		Thread.sleep(3000);
+		
+		((JavascriptExecutor)driver).executeScript("scroll(0,100)");
+		
+		((JavascriptExecutor)driver).executeScript("scroll(0,300)");
+		
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
+		
+	}
+
+}
